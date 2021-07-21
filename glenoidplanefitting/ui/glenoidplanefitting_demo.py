2b7a5b982321095ccea9b9d7a924eee502b373df
zcemaol <zcemaol@ucl.ac.uk>
2021-07-21 16:23:59 +0100

    Issue #2 fit a new plane

--- glenoidplanefitting/ui/glenoidplanefitting_demo.py
+++ glenoidplanefitting/ui/glenoidplanefitting_demo.py
@@ -8,7 +8,7 @@ from glenoidplanefitting.algorithms import plane_fitting
 def run_demo(model_file_name, output=""):
     """ Run the application """
     model = VTKSurfaceModel(model_file_name, [1., 0., 0.])
-    points = [[-57.8, 190.2, -76.9],[-117.7, 112.4, -96.8],[-92.5, 219.2, -162.1]]
+    points = [[-125.21, 122.57, -90.34],[-91.80, 226.94, -159.55],[-61.77, 193.93, -75.36]]
     return_meta = True
     result = plane_fitting.fit_plane_to_points(points,return_meta)
                                                      
@@ -20,10 +20,13 @@ def run_demo(model_file_name, output=""):
     if output != "":
 
         plane = vtk.vtkPlaneSource()
+        plane.SetOrigin(-100,-100,0)
+        plane.SetPoint1(-100,100,0)
+        plane.SetPoint2(100,-100,0)
         plane.SetCenter(result[1]) #result of center calc
         plane.SetNormal(result[2]) #result of normal vector
-        plane.SetXResolution(600)
-        plane.SetYResolution(600)
+        plane.SetXResolution(100)
+        plane.SetYResolution(100)
         
         writer = vtk.vtkXMLPolyDataWriter()
         writer.SetFileName(output)
