b12e68c2e743c2054f110c28d8ddce756399d4fc
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-11-23 16:18:19 +0000

    Issue #14 tidied up friedman

--- glenoidplanefitting/algorithms/friedman.py
+++ glenoidplanefitting/algorithms/friedman.py
@@ -1,5 +1,3 @@
-
-
 """
 This is an implentation of Friedman's method, see
 
@@ -8,52 +6,57 @@ The use of computerized tomography in the measurement
 of glenoid version. J Bone and Joint Surg Am 1992; 74: 1032–7.
 
 """
+
 import vtk
 import numpy as np
 import math
 
-
-def createFriedmanLine(p1,p2):
+def createFriedmanLine(point0,point1):
 
     """
     Determines the second point needed to form the Friedman line
-    
-    :param p1: First point on glenoid line, anatomically defined as a point on the anterior margin of glenoid
-    :param p2: Second point on glenoid line anatomically defined as a point on the posterior margin of glenoid
-    
-    :returns: The midpoint of the glenoid line, or the second point of for the Friedman line
+
+    :param point0: First point on glenoid line, anatomically defined as a 
+        point on the anterior margin of glenoid
+    :param point1: Second point on glenoid line anatomically defined as a 
+        point on the posterior margin of glenoid
+
+    :returns: The midpoint of the glenoid line, which is the second point of 
+        the Friedman line
     """
 
-    midpoint_x = (p1[0] + p2[0])/2
-    midpoint_y = (p1[1] + p2[1])/2
-    pm = [midpoint_x, midpoint_y,p1[2]]
+    midpoint_x = (point0[0] + point1[0])/2
+    midpoint_y = (point0[1] + point1[1])/2
+    midpoint = [midpoint_x, midpoint_y,point0[2]]
+    return midpoint
 
-    return pm
-
-def FriedmanVersion(p1,pm,p3):
+def FriedmanVersion(glenoid0,friedman1,friedman0):
 
     """
     Determines the glenoid version using the Friedman line
-    
-    :param p1: First point on glenoid line, anatomically defined as a point on the anterior margin of the glenoid
-    :param pm: Second point on the Friedman line, anatomically defined as the midpoint of the glenoid fossa
-    :param p3: First point on the Friedman line, anatomically defined as the medial tip of the scapula
+
+    :param glenoid0: First point on glenoid line, anatomically defined as a 
+        point on the anterior margin of the glenoid
+    :param friedman1: Second point on the Friedman line, anatomically defined
+        as the midpoint of the glenoid fossa
+    :param friedman0: First point on the Friedman line, anatomically defined
+        as the medial tip of the scapula
 
     :returns: The glenoid version (positive value indicates retroversion)
 
     """
 
-    a = np.array(p1)
-    b = np.array(pm)
-    c = np.array(p3)
+    glenoid0_arr = np.array(glenoid0)
+    friedman1_arr = np.array(friedman1)
+    friedman0_arr = np.array(friedman0)
 
-    ba = a - b
-    bc = c - b
+    glen_f1 = glenoid0_arr - friedman1_arr
+    f0_f1 = friedman0_arr - friedman1_arr
 
-    cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+    cosine_angle = np.dot(glen_f1, f0_f1) / (np.linalg.norm(glen_f1) * 
+            np.linalg.norm(f0_f1))
     radians = np.arccos(cosine_angle)
     math = vtk.vtkMath
     version = (math.DegreesFromRadians(radians))-90
-    
 
     return version
